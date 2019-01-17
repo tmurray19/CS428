031a9e6e35b6fd2f6141f5d512b0cde6ced0a3ec
tmurray19 <t.murray19@nuigalway.ie>
2019-01-17 09:38:07 +0000

    Initial Commit

---
+++ hw1.py
@@ -0,0 +1,14 @@
+# Open the data file in a read format
+f = open('data.txt', 'r')
+
+# y(i) = beta(0)+beta(1)*x(i)+beta(2)*x(i)^2+error(i)
+
+# Initialise x & y arrays
+Xi=[]
+Yi=[]
+
+# For-loop
+# Iterates through lines in file
+# If the number is an x, it's added to the Xi array
+# If the number is a y, it's added to the Yi Array
+for l in f:
